dae99673d7963a17aff5cfbc1f0d123d5be5d10b
Andrea <168139791+re-l-ux@users.noreply.github.com>
2024-11-21 10:57:58 +0000

    update

--- 4H/INF/Laboratorio/JavaScript/EsercizioA/EsercizioA.js
+++ 4H/INF/Laboratorio/JavaScript/EsercizioA/EsercizioA.js
@@ -2,4 +2,4 @@ let altezza = prompt("Inserisci l'altezza: ");
 let lunghezza = prompt("Inserisci la lungheza: ");
 let profondita = prompt("Inserisci la profondità: ");
 
-console.log("Il volume della figura è" + altezza*lunghezza*profondita)
+console.log("Il volume della figura è " + altezza*lunghezza*profondita)
---
+++ 4H/INF/Laboratorio/JavaScript/EsercizioB/EsercizioB.js
@@ -0,0 +1,11 @@
+let m = prompt("Inserisci il primo coefficente: ");
+let c = prompt("Inserisci il secondo coefficente: ");
+
+let x1 = -1;
+let x2 = 10;
+
+let y1 = (m * x1) + c;
+let y2 = (m * x2) + c;
+
+console.log("Il primo punto di intersezione è :" + x1);
+console.log("Il secondo punto di intersezione è :" + x2);
