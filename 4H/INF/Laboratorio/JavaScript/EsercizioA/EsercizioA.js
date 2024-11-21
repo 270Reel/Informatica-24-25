ceca4ef0ebe60a5b5b60a244125fb494e9a6316c
Andrea <168139791+re-l-ux@users.noreply.github.com>
2024-11-21 09:45:36 +0000

    update

---
+++ 4H/INF/Laboratorio/JavaScript/EsercizioA/EsercizioA.js
@@ -0,0 +1,5 @@
+let altezza = prompt("Inserisci l'altezza: ");
+let lunghezza = prompt("Inserisci la lungheza: ");
+let profondita = prompt("Inserisci la profondità: ");
+
+console.log("Il volume della figura è" + altezza*lunghezza*profondita)
