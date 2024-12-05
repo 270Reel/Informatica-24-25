9f24ab3e6eb540acf4a0f514c2d6827c81b75bda
Andrea <168139791+re-l-ux@users.noreply.github.com>
2024-12-05 10:11:17 +0000

    update

---
+++ 4H/INF/Teoria/C++/EserciziDiProva/CalcolaRettangolo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Rettangolo {
+
+    private:
+    double altezza;
+    double base;
+
+    public:
+    double setAltezza();
+    double setBase();
+    double Perimetro();
+    double Area();
+    void stampa();
+}
+
+int main(int argc, char argv[]) {
+
+    Rettangolo r1;
+    Rettangolo r2;
+
+    r1.setAltezza();
+}
+
