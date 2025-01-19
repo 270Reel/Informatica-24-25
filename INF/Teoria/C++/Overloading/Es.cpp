8bba5de7164c7b209c839630276cac7b50383265
Andrea <168139791+re-l-ux@users.noreply.github.com>
2025-01-19 17:56:37 +0000

    update

--- INF/Teoria/C++/Overloading/Es.cpp
+++ INF/Teoria/C++/Overloading/Es.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -16,13 +17,13 @@ class Rettangolo
         base    = 0;
     }
 
-    Rettangolo(altezza){
+    Rettangolo(double altezza){
         this->altezza = altezza;
         base          = altezza;
 
     }
 
-    Rettangolo(altezza, base){
+    Rettangolo(double altezza,double  base){
         this->altezza = altezza;
         this->base    = base;
     }
@@ -31,21 +32,48 @@ class Rettangolo
         cout << "Distruttore chiamato" <<endl;
     }
 
-    void setBase(base) {
+    void setBase(double base) {
         this->base = base;
     }
 
-    
-    void setBase(base) {
-        this->base = base;
+    double Perimetro() {
+        return (base + altezza) * 2;
     }
-}
+
+    double Area() {
+        return base * altezza;
+    }
+
+    double Diagonale() {
+        return sqrt((base*base) + (altezza*altezza));
+    }
+
+    void stampa() {
+        cout<<"Il valore dell'altezza e' "<<altezza<<endl;
+        cout<<"Il valore della base e' "<<base<<endl;
+        cout<<"Il valore delle perimetro e' "<<Perimetro()<<endl;
+        cout<<"Il valore dell'area e' "<<Area()<<endl;
+        cout<<"Il valore della diagonale "<<Diagonale()<<endl;
+    }
+};
 
 int main() {
 
     Rettangolo r1;
+    cout<<"[VALORI DEL PRIMO RETTANGOLO]"<<endl;
+    cout<<"-----------------------------"<<endl;
+    r1.stampa();
+
+
     Rettangolo r2(3);
+    cout<<endl<<"[VALORI DEL SECONDO RETTANGOLO]"<<endl;
+    cout<<"-----------------------------"<<endl;
+    r2.stampa();
+
     Rettangolo r3(7,9);
+    cout<<endl<<"[VALORI DEL TERZO RETTANGOLO]"<<endl;
+    cout<<"-----------------------------"<<endl;
+    r3.stampa();
 
 
 }
