909754714e0b6dff779b2b641ffc80fe33c73b57
Andrea <168139791+270Reel@users.noreply.github.com>
2025-02-10 09:41:12 +0000

    update

---
+++ INF/Teoria/C++/EreditàGerarchia/Quadrilatero.h
@@ -0,0 +1,40 @@
+#ifndef QUADRILATERO_H
+#define QUADRILATERO_H
+
+#include <iostream>
+#include <cmath>
+
+//Classe Padre
+class Quadrilatero {
+
+//Variabili accessibili solo dalle classi figlio
+private:
+    double lato1, lato2, lato3,lato4;
+
+public:
+
+    //Costruttore
+    Quadrilatero(double lato1, double lato2, double lato3, double lato4);
+    Quadrilatero(double lato1, double lato2);
+
+    //Metodi calcolatori
+    double Perimetro();
+    //double Area();
+
+    //Getters
+    double getLato1();
+    double getLato2();
+    double getLato3();
+    double getLato4();
+
+    //Setters
+    void setLato1(double lato);
+    void setLato2(double lato);
+    void setLato3(double lato);
+    void setLato4(double lato);
+
+    //Metodo stampa
+    void stampa();
+};
+
+#endif
